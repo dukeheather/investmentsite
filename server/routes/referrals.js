494bc44e7756a43f27a8331a954a7edcbf1e7b71
dukeheather <dexstroidmlbb@gmail.com>
2025-05-24 12:37:41 +0530

    ficing

--- server/routes/referrals.js
+++ server/routes/referrals.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const auth = require('../../middleware/auth');
+const auth = require('../middleware/auth');
 const crypto = require('crypto');
 
 // Generate a unique referral code
